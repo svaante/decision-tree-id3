fcf2e6173d48e04012533cd75a35f4d0d62f08ea
Flytet <otto.nordander@gmail.com>
2017-04-05 15:05:55 +0200

    Added initial data tests

--- id3/id3.py
+++ id3/id3.py
@@ -6,7 +6,7 @@ from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import euclidean_distances
-from data import load_contact_lenses
+from .data import load_contact_lenses
 
 
 class TemplateEstimator(BaseEstimator):
