ee9b887ed0582ce4572db86245275e6e830a0b7b
svaante <svaante@gmail.com>
2017-04-06 14:47:13 +0200

    circle ci stuff

--- id3/node.py
+++ id3/node.py
@@ -3,10 +3,29 @@ class Node():
 
     Parameters
     ----------
-    values : list of values to be saved to be saved by the node
-    is_feature : list of values to be saved to be saved by the node
+    name : name of the node should be either class or feature name
+    details : list of values to be saved to be saved by the node
+    estimator : label estimator
+    is_feature : if current node represent a feature or classification
     """
-    def __init__(self, values, is_feature=True):
-        self.values = values
+    def __init__(self,
+                 name,
+                 details,
+                 estimator,
+                 is_feature=True):
+        self.name = name
+        self.details = details
+        self.estimator = estimator
+        self.is_feature = is_feature
+        self.children = list()
 
-    #def add_child
+    def add_child(self, node, edge_value):
+        """ Add a child to node
+
+        Parameters
+        ----------
+        node : child node to be added
+        edge_value : attribute value for the edge
+        is_feature : if current node represent a feature or classification 
+        """
+        self.children.append((node, edge_value))
