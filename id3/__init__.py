135944b2e2ad06da523b6f043d8d5208ee0a9f19
svaante <svaante@gmail.com>
2017-04-20 14:38:24 +0200

    removed pdfexport

--- id3/__init__.py
+++ id3/__init__.py
@@ -1,7 +1,7 @@
 from .id3 import Id3Estimator
 from . import id3
 from .pruner import BasePruner, ErrorPruner, CostPruner
-from .export import export_graphviz, export_pdf
+from .export import export_graphviz
 
 __all__ = ['Id3Estimator',
            'TemplateClassifier',
