2d9dc70d53b5b00dfa3ed739a0c29d784e1b2064
Flytet <otto.nordander@gmail.com>
2017-04-24 18:27:45 +0200

    Added test for load_data

--- id3/data/load_data.py
+++ id3/data/load_data.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import os
+import io
 
 data_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -32,8 +33,7 @@ def load_data(path, nominal=True):
 
     """
     path = os.path.join(data_dir, path)
-    print(path)
-    with open(path, 'r', encoding='utf8') as f:
+    with io.open(path, 'r', encoding='utf8') as f:
         data_file = csv.reader(f)
         temp = next(data_file)
         n_samples = int(temp[0])
@@ -41,7 +41,7 @@ def load_data(path, nominal=True):
         target_names = np.array(temp[2:])
         data_type = 'a25' if nominal else np.int
         data = np.empty((n_samples, n_features), data_type)
-        target = np.empty((n_samples,), dtype=data_type)
+        target = np.empty((n_samples, 1), dtype=data_type)
 
         for i, ir in enumerate(data_file):
             data[i] = np.asarray(ir[:-1], dtype=data_type)
