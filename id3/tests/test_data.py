7ab9d3e7103f88392fa74a3668250309f1ae38c4
Flytet <otto.nordander@gmail.com>
2017-04-24 18:31:03 +0200

    Added the actual files for test_data....

---
+++ id3/tests/test_data.py
@@ -0,0 +1,14 @@
+from numpy.testing import assert_almost_equal, assert_raises
+import numpy as np
+from id3.data import load_data
+import uuid
+
+X = np.arange(20).reshape(10, 2)
+y = np.arange(10).reshape(10, 1)
+
+
+def test_load_data():
+    assert_raises(IOError, load_data.load_data, str(uuid.uuid4()))
+    X_, y_, _ = load_data.load_data("test.csv", nominal=False)
+    assert_almost_equal(X, X_)
+    assert_almost_equal(y, y_)
