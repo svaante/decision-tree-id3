16b7344c7c94669487c600d23b048cbd1407d0cf
svaante <svaante@gmail.com>
2017-04-25 16:03:54 +0200

    unique performace updated

--- id3/utils/array_operations.py
+++ id3/utils/array_operations.py
@@ -2,5 +2,6 @@ import numpy as np
 
 
 def unique(array):
-    uni, inv = np.unique(array, return_inverse=True)
-    return uni, np.bincount(inv)
+    counts = np.bincount(array)
+    mask = counts != 0
+    return np.nonzero(mask)[0], counts[mask]
