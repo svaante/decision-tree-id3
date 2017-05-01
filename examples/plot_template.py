135be47ccc22289daa9c7695a0a458a90fae27cb
Flytet <otto.nordander@gmail.com>
2017-05-01 13:18:29 +0200

    Removed old examples

--- examples/plot_template.py
+++ examples/plot_template.py
@@ -6,12 +6,12 @@ Plotting Template Estimator
 An example plot of :class:`skltemplate.template.TemplateEstimator`
 """
 import numpy as np
-from skltemplate import TemplateEstimator
+from id3 import Id3Estimator
 from matplotlib import pyplot as plt
 
 X = np.arange(100).reshape(100, 1)
 y = np.zeros((100, ))
-estimator = TemplateEstimator()
+estimator = Id3Estimator()
 estimator.fit(X, y)
 plt.plot(estimator.predict(X))
 plt.show()
