6be3288843ea38b814298ad60a661ac38b1a7d47
svaante <svaante@gmail.com>
2017-05-04 22:35:46 +0200

    export_text

--- id3/__init__.py
+++ id3/__init__.py
@@ -1,6 +1,7 @@
 from .id3 import Id3Estimator
 from . import id3
 from .export import export_graphviz
-from . import splitter
+from .export import export_text
 
-__all__ = ['Id3Estimator', 'id3', 'export_graphviz', 'splitter']
+__all__ = ['Id3Estimator', 'id3', 'export_graphviz', 'export_text',
+           'splitter']
