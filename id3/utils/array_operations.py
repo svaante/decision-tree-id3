d2f3d2b27e059b3f5eb489cdcc59eb2f247df9fd
svaante <svaante@gmail.com>
2017-05-14 01:39:16 +0200

    astype fix

--- id3/utils/array_operations.py
+++ id3/utils/array_operations.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def unique(array):
     if getattr(array, "dtype", None) == np.float32:
-        array = array.astype(int, copy=False)
+        array = array.astype(int)
     counts = np.bincount(array)
     mask = counts != 0
     return np.nonzero(mask)[0], counts[mask]
